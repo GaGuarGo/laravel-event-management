401d532aec7b5a1d306ab745030c4fee6d97f79f
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-06-07 20:12:13 -0300

    Optional Relation Loading

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -16,7 +16,32 @@ class EventController extends Controller
     {
         //Processo de Serializing-> transformando em JSON
 
-        return EventResource::collection(Event::with('user')->paginate());
+        $query = Event::query();
+        $relations = ['user', 'attendees', 'attendees.user'];
+
+        foreach ($relations as $relation) {
+            $query->when(
+                $this->shouldIncludeRelation($relation),
+                fn($q) => $q->with($relation)
+            );
+        }
+
+        return EventResource::collection(
+            $query->latest()->paginate()
+        );
+    }
+
+    protected function shouldIncludeRelation(string $relation): bool
+    {
+        $include = request()->query('include');
+
+        if (!$include) {
+            return false;
+        }
+
+        $relations = array_map('trim', explode(',', $include));
+
+        return in_array($relation, $relations);
     }
 
     /**
