521663b48e17f2d20aacfaf9399f46c9eab66923
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-06-07 18:27:44 -0300

    Storing Data and Validation

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
@@ -12,7 +13,9 @@ class EventController extends Controller
      */
     public function index()
     {
-        //
+
+        //Processo de Serializing-> transformando em JSON
+        return Event::all();
     }
 
     /**
@@ -20,15 +23,28 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+    //Colocar Headers para retornar um JSON com as validações
+
+        $event = Event::create(
+            [...$request->validate([
+                'name' => 'required|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'required|date',
+                'end_time' => 'required|date|after:start_time',
+            ]),
+                'user_id' => 1]
+        );
+
+        return $event;
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Event $event)
     {
-        //
+        return $event;
     }
 
     /**
