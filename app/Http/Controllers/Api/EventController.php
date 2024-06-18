468c5c8a6357e4d3cb83b570fc8f3ae381fb138b
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-06-18 20:39:16 -0300

    Protecting Routes

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -15,6 +15,12 @@ class EventController extends Controller
 
     private array $relations = ['user', 'attendees', 'attendees.user'];
 
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except(['index', 'show']);
+    }
+
+
     /**
      * Display a listing of the resource.
      */
@@ -46,7 +52,8 @@ class EventController extends Controller
                 'start_time' => 'required|date',
                 'end_time' => 'required|date|after:start_time',
             ]),
-                'user_id' => 1]
+                'user_id' => $request->user()->id,
+                ]
         );
 
         return new EventResource($this->loadRelationships($event, $this->relations));
