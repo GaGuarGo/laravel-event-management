3bf96cf5cab288a2afd9dda4d60a2848f1cc2b07
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-06-19 20:27:50 -0300

    Authorization with Policies

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -19,6 +19,7 @@ class EventController extends Controller
     public function __construct()
     {
         $this->middleware('auth:sanctum')->except(['index', 'show']);
+        $this->authorizeResource(Event::class, 'event');
     }
 
 
@@ -79,7 +80,7 @@ class EventController extends Controller
 //            abort(403, 'You are not authorized to update this event.');
 //        }
 
-        $this->authorize("update-event", $event);
+//        $this->authorize("update-event", $event);
 
         $data = $request->validate([
             'name' => 'sometimes|string|max:255',
