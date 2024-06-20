74c3e8f2ec5c4ec8ffadffa0f18ef392414b1d89
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-06-20 18:57:00 -0300

    Throttling the API

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -19,6 +19,8 @@ class AttendeeController extends Controller
     public function __construct()
     {
         $this->middleware('auth:sanctum')->except(['index', 'show', 'update']);
+
+        $this->middleware('throttle:api')->only(['store', 'destroy', 'update']);
         $this->authorizeResource(Attendee::class, 'attendee');
     }
 
@@ -39,7 +41,7 @@ class AttendeeController extends Controller
     public function store(Request $request, Event $event)
     {
         $attendee = $this->loadRelationships($event->attendees()->create([
-            'user_id' => 1
+            'user_id' => $request->user()->id,
         ]));
 
         return new AttendeeResource($attendee);
--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -19,6 +19,7 @@ class EventController extends Controller
     public function __construct()
     {
         $this->middleware('auth:sanctum')->except(['index', 'show']);
+        $this->middleware('throttle:api')->only(['store', 'destroy']);
         $this->authorizeResource(Event::class, 'event');
     }
 
